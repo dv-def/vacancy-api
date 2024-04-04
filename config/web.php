2a48f8900d4cb2597bae44caa7d5b5b89f6ccb6f
Dmitry Negrobov <ndv.def@gmail.com>
2024-04-04 22:52:09 +0300

    add json parser in config

--- config/web.php
+++ config/web.php
@@ -15,6 +15,9 @@ $config = [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'aEZFauR1RW2Ghjldslh8kTgUXKC9Bl5Y',
+            'parsers' => [
+                'application/json' => 'yii\web\JsonParser',
+            ]
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
